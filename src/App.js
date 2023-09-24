dc759618c3e7bbfafb39fd70013ef706001cef75
BrooklynP <Brooklynpedley@gmail.com>
2023-09-24 14:26:32 +0100

    setup routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { HomePage } from './pages';
 
 function App() {
+
+  let component;
+  switch (window.location.pathname) {
+    case "/":
+      component = <HomePage />;
+      break;
+    default:
+      component = <HomePage />
+      break;
+  }
   return (
     <div className="App">
-
+      {component}
     </div>
   );
 }
