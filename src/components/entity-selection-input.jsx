e7375f37e7ade495f6bc2fe0e0a7e0ddb21c3895
BrooklynP <Brooklynpedley@gmail.com>
2023-09-25 20:47:21 +0100

    cleanup

--- src/components/entity-selection-input.jsx
+++ src/components/entity-selection-input.jsx
@@ -4,7 +4,7 @@ import "./entity-selection-input.css"
 function EntitySelectionInput({ options, selectedOption, id, onChangeFunction }) {
 
     const optionsList = options.map((option, index) =>
-        <li key={index} className={option === selectedOption ? 'selected' : ''} onClick={(e) => { onChangeFunction(e, option) }}>{option}</li>
+        <li key={index} className={option.value === selectedOption ? 'selected' : ''} onClick={(e) => { onChangeFunction(e, option.value) }}>{option.label}</li>
     )
 
     return (
